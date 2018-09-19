220632c89320ca8e1d67f5a511d6decfd9296469
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2018-09-20 09:22:42 +1200

    Update application description

--- cmd/root.go
+++ cmd/root.go
@@ -34,16 +34,11 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "flexi-pass",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Manage flexi pass usage in a co-working space.",
+	Long: `Flexi Pass is a simple tool to help co-working administrators manage their users
+	that have a flexible subscription.  It is a simple command line tool that you can use
+	to add users, set subscription periods and see how much of their subscription has been
+	used in a given period.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
