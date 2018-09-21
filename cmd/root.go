8296cbd8b871a08c94e812addd920695304fd41d
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2018-09-21 20:42:42 +1200

    tidy-up root command
    Correct command description.  Fix variable name and flag.  Set default
    value for data dir.

--- cmd/root.go
+++ cmd/root.go
@@ -23,22 +23,22 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var dataDir string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "flexi-pass",
 	Short: "Manage flexi pass usage in a co-working space.",
 	Long: `Flexi Pass is a simple tool to help co-working administrators manage their users
-	that have a flexible subscription.  It is a simple command line tool that you can use
-	to add users, set subscription periods and see how much of their subscription has been
-	used in a given period.`,
+that have a flexible subscription.  It is a simple command line tool that you can
+use to add users, set subscription periods and see how much of their subscription
+has been used in a given period.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -51,40 +51,22 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	initConfig()
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.flexi-pass.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&dataDir, "data-dir", "d", dataDir, "application's data directory.")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".flexi-pass" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".flexi-pass")
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	dataDir = path.Join(home, ".suburban", "flexi-pass")
 }
