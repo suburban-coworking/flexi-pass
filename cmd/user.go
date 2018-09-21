eaf046fa84333437e86da50e87cc38b89e7d13ff
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2018-09-21 22:51:11 +1200

    Add survey package
    Survey prompts the user for input.  Started to make use of this in the
    new 'user-add' command (may rename later).

--- cmd/user.go
+++ cmd/user.go
@@ -17,34 +17,50 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/AlecAivazis/survey"
 	"github.com/spf13/cobra"
 )
 
-// userCmd represents the user command
-var userCmd = &cobra.Command{
-	Use:   "user",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+// userAdd represents the user command
+var userAdd = &cobra.Command{
+	Use:   "user-add",
+	Short: "Add a new flexi-pass user",
+	Long:  `Create a new flexi-pass user in the system.`,
+	Run:   runCommand,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("user called")
+// Setup a series of questions to get the user's details.
+var userQuestions = []*survey.Question{
+	{
+		Name:      "name",
+		Prompt:    &survey.Input{Message: "Name"},
+		Validate:  survey.Required,
+		Transform: survey.Title,
+	},
+	{
+		Name:      "email",
+		Prompt:    &survey.Input{Message: "Email"},
+		Validate:  survey.Required,
+		Transform: survey.ToLower,
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(userCmd)
+	rootCmd.AddCommand(userAdd)
+}
 
-	// Here you will define your flags and configuration settings.
+func runCommand(cmd *cobra.Command, args []string) {
+	answers := struct {
+		Name  string
+		Email string
+	}{}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// userCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// perform the questions
+	err := survey.Ask(userQuestions, &answers)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// userCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	fmt.Printf("%s, %s.", answers.Name, answers.Email)
 }
